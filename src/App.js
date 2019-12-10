df8b18d5f36de793e011b31b03f19830913884af
Janessa Garrow <janessa.garrow@gmail.com>
2019-12-09 23:48:34 -0700

    Clean up form. Complete stretch edit functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,89 @@
 import React, { useState } from "react";
+import styled from "@emotion/styled";
+
+import { FaEdit } from "react-icons/fa";
 
 import Form from "./Form";
 
 import "./App.css";
 
+const PersonContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(175px, 1fr));
+    grid-gap: 1rem;
+    margin: 2rem auto 0;
+`;
+
+const PersonWrapper = styled.div`
+    margin: 0 auto;
+    position: relative;
+    width: fit-content;
+    box-sizing: border-box;
+    padding: 0.5rem;
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+`;
+
+const EditIcon = styled(FaEdit)`
+    position: absolute;
+    right: 5px;
+    top: 5px;
+    cursor: pointer;
+`;
+
 function App() {
     const [teamMembers, setTeamMembers] = useState([]);
+    const [memberToEdit, setMemberToEdit] = useState(-1); //index of member we are editing
+
+    const handleEditIconClick = member => {
+        setMemberToEdit(member);
+    };
+
+    const addNewTeamMember = teamMember => {
+        const newTeamMembers = [...teamMembers, teamMember];
+        setTeamMembers(newTeamMembers);
+    };
+
+    const editMember = editedMember => {
+        const members = [...teamMembers];
+        members[memberToEdit] = editedMember;
+        setTeamMembers(members);
+        setMemberToEdit(-1);
+    };
+
+    const submitForm = teamMember => {
+        if (memberToEdit > -1) {
+            editMember(teamMember);
+        } else {
+            addNewTeamMember(teamMember);
+        }
+    };
 
     return (
-        <div>
-            <Form teamMembers={teamMembers} setTeamMembers={setTeamMembers} />
-            {teamMembers !== [] && teamMembers.map((person, index) => (
-                <div key={index}>
-                    <h2>Name: {person.name}</h2>
-                    <p>Email: {person.email}</p>
-                    <p>Role: {person.role}</p>
-                </div>
-            ))}
+        <div className="App">
+            <Form submitForm={submitForm} />
+            <PersonContainer>
+                {teamMembers !== [] &&
+                    teamMembers.map((person, index) => (
+                        <PersonWrapper key={index}>
+                            <EditIcon
+                                onClick={() => handleEditIconClick(index)}
+                            />
+                            <div>
+                                <h2>Name: {person.name}</h2>
+                                <p>Email: {person.email}</p>
+                                <p>Role: {person.role}</p>
+                            </div>
+                        </PersonWrapper>
+                    ))}
+            </PersonContainer>
+            {memberToEdit > -1 && (
+                <Form
+                    submitForm={submitForm}
+                    memberToEdit={teamMembers[memberToEdit]}
+                />
+            )}
         </div>
     );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,30 +1,86 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
 
-export default ({ teamMembers, setTeamMembers }) => {
-    const [teamMember, setTeamMember] = useState([]);
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+
+const InputContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: ;
+`;
+
+const SubmitButton = styled.button`
+    width: 50px;
+    height: 30px;
+    margin: 1rem auto 0;
+`;
+
+export default ({ submitForm, addNewTeamMember, memberToEdit, editMember }) => {
+    const [teamMember, setTeamMember] = useState({
+        name: "",
+        email: "",
+        role: ""
+    });
+
+    useEffect(() => {
+        if (memberToEdit) {
+            setTeamMember(memberToEdit);
+        }
+    }, [memberToEdit]);
 
     const handleChange = e => {
-        let value = e.target.value;
-        setTeamMember({ ...teamMember, [e.target.name]: value });
+        setTeamMember({ ...teamMember, [e.target.name]: e.target.value });
     };
 
     const handleSubmit = e => {
         e.preventDefault();
-        setTeamMembers([...teamMembers, teamMember]);
-        console.log("Submitting: ", teamMember);
-    }
-
-    console.log(teamMember);
+        submitForm(teamMember);
+        setTeamMember({ name: "", email: "", role: "" });
+    };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name: </label>
-            <input type="text" name="name" id="name" value={teamMember.name} onChange={handleChange}/>
-            <label htmlFor="email">Email: </label>
-            <input type="email" name="email" id="email" value={teamMember.email} onChange={handleChange}/>
-            <label htmlFor="role">Role: </label>
-            <input type="text" name="role" id="role" value={teamMember.role} onChange={handleChange}/>
-            <button>Submit</button>
-        </form>
+        <Form onSubmit={handleSubmit}>
+            <InputContainer>
+                <label htmlFor="name">
+                    Name:
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Name"
+                        value={teamMember.name}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <label htmlFor="email">
+                    Email:
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        value={teamMember.email}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <label htmlFor="role">
+                    Role:
+                    <input
+                        type="text"
+                        name="role"
+                        id="role"
+                        placeholder="Role"
+                        value={teamMember.role}
+                        onChange={handleChange}
+                    />
+                </label>
+            </InputContainer>
+            <SubmitButton type="submit">Submit</SubmitButton>
+        </Form>
     );
 };
