89dcba64e5a038f2743dfb4a380f0820a4643b8c
Janessa Garrow <janessa.garrow@gmail.com>
2019-12-10 21:14:33 -0700

    Add some styling to form

--- src/Form.js
+++ src/Form.js
@@ -4,6 +4,8 @@ import styled from "@emotion/styled";
 const Form = styled.form`
     display: flex;
     flex-direction: column;
+    justify-content: space-evenly;
+    height: 300px;
 `;
 
 const InputContainer = styled.div`
@@ -13,12 +15,23 @@ const InputContainer = styled.div`
 `;
 
 const SubmitButton = styled.button`
-    width: 50px;
-    height: 30px;
+    width: 80px;
+    height: 35px;
     margin: 1rem auto 0;
+    border-radius: 8px;
+    box-sizing: border-box;
+    padding: 10px;
+    font-size: 1rem;
+    line-height: 0;
+    box-shadow: 1px 1px 5px 0px grey;
+    cursor: pointer;
+
+    &:hover {
+        background-color: lightgray;
+    }
 `;
 
-export default ({ submitForm, addNewTeamMember, memberToEdit, editMember }) => {
+export default ({ submitForm, memberToEdit }) => {
     const [teamMember, setTeamMember] = useState({
         name: "",
         email: "",
@@ -43,43 +56,43 @@ export default ({ submitForm, addNewTeamMember, memberToEdit, editMember }) => {
 
     return (
         <Form onSubmit={handleSubmit}>
-            <InputContainer>
-                <label htmlFor="name">
-                    Name:
-                    <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        placeholder="Name"
-                        value={teamMember.name}
-                        onChange={handleChange}
-                    />
-                </label>
+            {/* <InputContainer> */}
+            <label htmlFor="name">
+                Name:
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Name"
+                    value={teamMember.name}
+                    onChange={handleChange}
+                />
+            </label>
 
-                <label htmlFor="email">
-                    Email:
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="Email"
-                        value={teamMember.email}
-                        onChange={handleChange}
-                    />
-                </label>
+            <label htmlFor="email">
+                Email:
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    value={teamMember.email}
+                    onChange={handleChange}
+                />
+            </label>
 
-                <label htmlFor="role">
-                    Role:
-                    <input
-                        type="text"
-                        name="role"
-                        id="role"
-                        placeholder="Role"
-                        value={teamMember.role}
-                        onChange={handleChange}
-                    />
-                </label>
-            </InputContainer>
+            <label htmlFor="role">
+                Role:
+                <input
+                    type="text"
+                    name="role"
+                    id="role"
+                    placeholder="Role"
+                    value={teamMember.role}
+                    onChange={handleChange}
+                />
+            </label>
+            {/* </InputContainer> */}
             <SubmitButton type="submit">Submit</SubmitButton>
         </Form>
     );
